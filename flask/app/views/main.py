66861fbb66efc503b4121c5337887eafb6689e6d
sXinker <zmfflr156@naver.com>
2024-11-13 12:44:29 +0900

    변경사항 수정

--- flask/app/views/main.py
+++ flask/app/views/main.py
@@ -1,30 +1,3 @@
-<<<<<<< Updated upstream
-from flask import Blueprint
-from flask import render_template, request, session, flash, current_app
-
-
-bp = Blueprint('main', __name__)
-
-
-@bp.route("/", methods=['GET', 'POST'])
-def index():
-    return render_template('index.html')
-
-<<<<<<< HEAD
-    if request.method == 'POST':
-        code = request.form.get('pass-code')
-
-        if code == current_app.config["PASS_CODE"]:
-            session['checked_ticket'] = True
-            return render_template('index.html')
-        else:
-            flash("잘못된 코드입니다.")
-    else:
-        if session.get('checked_ticket'):
-            return render_template('index.html')
-
-    return render_template('gate.html')
-=======
 from app.models import User
 from flask_login import login_user, current_user
 from flask import Blueprint
@@ -46,9 +19,8 @@ def module_test():
     
     session["point"] = 1000
     return render_template("test/testpage01.html")
->>>>>>> Stashed changes
-=======
+
 @bp.route("/test", methods=["GET", "POST"])
 def module_test():
     return render_template("test/testpage01.html")
->>>>>>> d5a6d6f08f3a58e5cfa408aece902ee5d159314f
+
