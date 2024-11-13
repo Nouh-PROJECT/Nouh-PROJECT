787f4a25730750d4748d3edb06384377d11bc5ab
woongmur <mini10001100101@gmail.com>
2024-11-13 11:18:53 +0900

    네비게이션 추가

--- flask/app/views/main.py
+++ flask/app/views/main.py
@@ -1,4 +1,8 @@
 from flask import Blueprint
+from flask import render_template, Blueprint, session
+from flask_login import login_user, current_user
+from app.models import User
+from app.utils.db import execute_query
 from flask import render_template, request, session, flash, current_app
 
 
@@ -7,6 +11,13 @@ bp = Blueprint('main', __name__)
 
 @bp.route("/", methods=['GET', 'POST'])
 def index():
+    login_user(User(2, "게스트", "guest"))
+    rows = execute_query(r"SELECT point FROM membership WHERE id=%s", (current_user.id,))
+    if rows:
+        point = rows[0]
+        session["point"] = point
+    else:
+        session["point"] = 0
     return render_template('index.html')
 
 @bp.route("/test", methods=["GET", "POST"])
